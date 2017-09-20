af1fe868672dd2e28bd318cc5f0de269e6356a76
licencep <licencep@infotp17.cnam.fr>
2017-09-20 11:12:46 +0200

    merde

--- essai2.c
+++ essai2.c
@@ -8,9 +8,9 @@ main(){
 	pid =fork();
 	if(pid == 0){
 		execl("/bin/ls","ls" ,"-l" ,NULL);
-		printf("fils : pid du fils %d : celui du pere %d \n",getpid(), getppid());
+		printf(" je suis lefils mon pid %d : celui du pere %d \n",getpid(), getppid());
 	}else{
-		printf("pere : pid du pere %d,  pid du fils %d\n ",getpid(), pid);
+		printf("Je suis le pere mon  %d,  pid du fils %d\n ",getpid(), pid);
 		wait();
 	}
 }
