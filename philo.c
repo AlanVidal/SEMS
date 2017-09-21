c9068d3f39e04c379806723192df547b0750576d
licencep <licencep@infotp17.cnam.fr>
2017-09-21 11:20:05 +0200

    minor

--- philo.c
+++ philo.c
@@ -3,18 +3,25 @@
 #include <pthread.h>
 #include <stdlib.h>
  
-pthread_mutex_t fourchette[4];
+pthread_mutex_t fourchette[5];
 
 void fourchetteLock( void *i){
 	int iTmp = *((int*)i);
 
 	pthread_mutex_lock(&fourchette[iTmp]);
-	printf("Lock %d\n", iTmp);
-        pthread_mutex_lock(&fourchette[iTmp-1%4]);
-	sleep(1);	 
+	printf("Philo %d prend fourchette de droite\n", iTmp);
+        sleep(1);
+        pthread_mutex_lock(&fourchette[(iTmp-1)%4]);
+        printf("Philo %d prend fourchette de gauche\n", iTmp);
+
+
+	sleep(2);	 
         pthread_mutex_unlock(&fourchette[iTmp]);
-        printf("Lock %d\n", iTmp);
-        pthread_mutex_unlock(&fourchette[iTmp-1%4]);
+        printf("Philo %d rend fourchette de droite\n", iTmp);
+        sleep(1);
+	 pthread_mutex_unlock(&fourchette[(iTmp-1)%4]);
+        printf("Philo %d rend fourchette de gauche\n", iTmp);
+
 	sleep(2);
 
 }
@@ -23,7 +30,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(){
 
-        int nbrPhilo = 4;
+        int nbrPhilo = 5;
 	pthread_t num_thread[nbrPhilo];
 	int compteur[nbrPhilo];
 	for(int i = 0; i < nbrPhilo ; i++){ compteur[i]=i;  }
